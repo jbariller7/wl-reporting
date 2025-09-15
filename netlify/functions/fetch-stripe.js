a7a612b0691328e9d7e49dc84ab23ba6dd3960a0
jbariller7 <116109740+jbariller7@users.noreply.github.com>
2025-09-15 00:00:44 -0700

    Create fetch-stripe.js

---
+++ netlify/functions/fetch-stripe.js
@@ -0,0 +1,10 @@
+import { parseRange, ok, bad } from "../../lib/util.js";
+import { etlStripe } from "../../lib/etl.js";
+
+export const handler = async (event) => {
+  if (event.httpMethod !== "POST") return bad("Use POST");
+  const body = event.body ? JSON.parse(event.body) : {};
+  const range = parseRange(body);
+  const res = await etlStripe(range);
+  return ok(res);
+};
